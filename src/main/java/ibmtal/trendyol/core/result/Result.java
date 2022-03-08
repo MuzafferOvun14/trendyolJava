2f9184138240cda2351ca3054923c5bbc6cc73eb
MuzafferOvun14 <ibmtalbilisim@gmail.com>
2022-03-08 16:04:30 +0300

    sdf
    fsdfs

--- src/main/java/ibmtal/trendyol/core/result/Result.java
+++ src/main/java/ibmtal/trendyol/core/result/Result.java
@@ -1,5 +1,36 @@
 package ibmtal.trendyol.core.result;
 
-public class Result {
+import java.util.ArrayList;
 
+public class Result<T> {
+	private Boolean success;
+	private ArrayList<ResultItem> errors;
+	private ArrayList<T> data;
+	public Result() {
+		super();
+	}
+	public Result(Boolean success, ArrayList<ResultItem> errors, ArrayList<T> data) {
+		super();
+		this.success = success;
+		this.errors = errors;
+		this.data = data;
+	}
+	public Boolean getSuccess() {
+		return success;
+	}
+	public void setSuccess(Boolean success) {
+		this.success = success;
+	}
+	public ArrayList<ResultItem> getErrors() {
+		return errors;
+	}
+	public void setErrors(ArrayList<ResultItem> errors) {
+		this.errors = errors;
+	}
+	public ArrayList<T> getData() {
+		return data;
+	}
+	public void setData(ArrayList<T> data) {
+		this.data = data;
+	}
 }
